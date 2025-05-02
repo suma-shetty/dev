3f708d50bcda1f78bc0e3cde851c0785fe62d82d
suma-shetty <sumashetty103@gmail.com>
2025-05-02 01:35:42 +0100

    using generic String program practice

---
+++ src/name.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class name {
+public static void main (String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("Rohit");
+    list.add("Akash");
+    list.add("PK");
+    System.out.println(list);
+}
+}
